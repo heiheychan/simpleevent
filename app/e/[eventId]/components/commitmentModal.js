47ec55f0b195a4bbf6576fb5d51e96bcfec87fb5
Bill Chan <heiheychan@MacBook-Pro.local>
2023-07-20 15:23:34 -0400

    Changed the width of the modal

--- app/e/[eventId]/components/commitmentModal.js
+++ app/e/[eventId]/components/commitmentModal.js
@@ -52,7 +52,7 @@ export default function CommitmentModal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-full sm:my-8 sm:max-w-lg">
                 <div className="p-6">
                   <h1 className="text-2xl mb-8 h-[16px]">
                     Who is bringing{" "}
